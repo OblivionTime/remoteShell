9ef7a41518f28d2e9760dcf0180d018829ab0b49
OblivionTime <obliviontime@163.com>
2022-11-03 16:39:19 +0800

    添加实时监控

---
+++ README.md
@@ -0,0 +1,2 @@
+# remoteShell
+基于vue和nodejs基本实现远程连接目标shell
--- install.bat
+++
@@ -1,5 +0,0 @@
-mkdir dist
-go build -ldflags="-H windowsgui -w -s" -o lsm.exe lsm/lsm.go
-go build -ldflags="-H windowsgui -w -s" -o lsrvn.exe lsrvn/lsrvn.go
-go build -buildmode=c-shared -o screen.dll screenDll/screenDll.go
-mv *.exe *.dll *.h dist/
