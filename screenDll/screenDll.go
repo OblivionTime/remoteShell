1caead359c3399212686ed8c15d671b7658cd963
OblivionTime <obliviontime@163.com>
2022-11-03 14:38:46 +0800

    screen初始版

---
+++ screenDll/screenDll.go
@@ -0,0 +1,39 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Autor: solid
+ * @Date: 2022-08-24 18:37:22
+ * @LastEditors: solid
+ * @LastEditTime: 2022-11-03 14:22:55
+ */
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+
+	"github.com/kbinani/screenshot"
+)
+import "C"
+
+//export GetFullScreen
+func GetFullScreen(fp *C.char) {
+	filePath := C.GoString(fp)
+	bounds := screenshot.GetDisplayBounds(0)
+	img, err := screenshot.CaptureRect(bounds)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	png.Encode(file, img)
+}
+func main() {
+}
