f619ab16f9230f14cb90a87cde43c24635dcefbc
OblivionTime <obliviontime@163.com>
2022-11-07 09:38:42 +0800

    添加文件管理

--- index.js
+++ index.js
@@ -4,13 +4,14 @@
  * @Autor: solid
  * @Date: 2022-10-31 10:04:38
  * @LastEditors: solid
- * @LastEditTime: 2022-11-01 18:05:35
+ * @LastEditTime: 2022-11-03 18:20:56
  */
 const url = require('url');
 var app = require('./app/app');
 var server = require("http").createServer(app);
 const device_ws_server = require('./deviceWs/deviceWs')();
 const screen_ws_server = require('./screenWs/screenWs')();
+const file_ws_server = require('./fileWs/fileWs')();
 // 协议提升
 server.on('upgrade', (request, socket, head) => {
     const pathname = url.parse(request.url).pathname;
@@ -22,6 +23,10 @@ server.on('upgrade', (request, socket, head) => {
     } else if (pathname === '/screen') {
         screen_ws_server.handleUpgrade(request, socket, head, socket => {
             screen_ws_server.emit('connection', socket, request);
+        });
+    } else if (pathname === '/file') {
+        file_ws_server.handleUpgrade(request, socket, head, socket => {
+            file_ws_server.emit('connection', socket, request);
         });
     }
 });
